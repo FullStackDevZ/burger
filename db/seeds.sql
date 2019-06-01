4f67b2cc3247a9eb4e2b8339059b0a010e3843c7
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-06-01 14:51:58 -0700

    devour

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Lounge Burger', false),
- ('Bison Burger', false), ('Turkey Burger', false), ('Jalapeno Burger', true),
-('Kiwilango Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Lounge Burger'),
+ ('Bison Burger'), ('Turkey Burger'), ('Jalapeno Burger'),
+('Kiwilango Burger');
